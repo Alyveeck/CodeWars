2f9f359529f2fd3994d13cb2a1161c27ee49125c
Alysson Vignochi Veeck <alyssonveeck@hotmail.com>
2022-12-15 12:08:01 -0300

    Create rockpaperscissors.java

---
+++ 8kyu/rockpaperscissors.java
@@ -0,0 +1,52 @@
+//Rock Paper Scissors
+//Let's play! You have to return which player won! In case of a draw return Draw!.
+
+//Examples(Input1, Input2 --> Output):
+
+//"scissors", "paper" --> "Player 1 won!"
+//"scissors", "rock" --> "Player 2 won!"
+//"paper", "paper" --> "Draw!"
+
+/*********SOLUÇÃO*********/
+
+public class Kata {
+  public static String rps(String p1, String p2) {
+    // your code
+    if(p1 == "rock"){
+      if(p2 == "scissors"){
+        return "Player 1 won!";
+      }
+      else if(p2 == "paper"){
+        return "Player 2 won!";
+      }
+      else{
+        return "Draw!";
+      }
+    }
+    else if(p1 == "paper"){
+       if(p2 == "scissors"){
+        return "Player 2 won!";
+      }
+      else if(p2 == "rock"){
+        return "Player 1 won!";
+      }
+      else{
+        return "Draw!";
+      }
+    }
+    else if(p1 == "scissors"){
+       if(p2 == "paper"){
+        return "Player 1 won!";
+      }
+      else if(p2 == "rock"){
+        return "Player 2 won!";
+      }
+      else{
+        return "Draw!";
+      }
+   }
+    else{
+      return "Draw";
+    }
+  }
+}
